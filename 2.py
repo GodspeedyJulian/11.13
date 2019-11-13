0fe498e989c11ca0c8ad4e07a549e1058233320d
Julian Ye <50823612+GodspeedyJulian@users.noreply.github.com>
2019-11-13 12:28:16 +0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+def rle_decode(data):
+    decode = ''
+    count = ''
+    for char in data:
+        # If the character is numerical...
+        if char.isdigit():
+            # ...append it to our count
+            count += char
+        else:
+            # Otherwise we've seen a non-numerical
+            # character and need to expand it for
+            # the decoding
+            decode += char * int(count)
+            count = ''
+    return decode
